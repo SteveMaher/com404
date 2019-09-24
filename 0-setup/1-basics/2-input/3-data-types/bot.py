8b76e47d6eebc15dd580199c33a9b3c6cac0a3ba
SteveMaher <4mahes86@solent.ac.uk>
2019-09-24 16:56:43 +0000

    Bmi Calculator

---
+++ 0-setup/1-basics/2-input/3-data-types/bot.py
@@ -0,0 +1,14 @@
+# BMI Calculator
+print("What is your name human?")
+name = input()
+print("How old are you (in years)?")
+age = int(input())
+print("How tall are you(in meters?)")
+height = float(input)
+print("How much do you weigh (in kilograms)?")
+weight = float(input())
+# Height Squared
+heightsquared = height*height
+#bmi weight divided by height squared
+bmi = weight/heightsquared
+print (name + "you are" + age "old and your bmi is" + bmi)
