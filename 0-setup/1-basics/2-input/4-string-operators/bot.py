d5aa286739f69585a3da198f1bbbd6cc89732f88
SteveMaher <4mahes86@solent.ac.uk>
2019-09-27 08:56:13 +0000

    Added code to demonstrate the use of string operators

--- 0-setup/1-basics/2-input/4-string-operators/bot.py
+++ 0-setup/1-basics/2-input/4-string-operators/bot.py
@@ -1,13 +1,19 @@
 # Health meter
-print("Please enter the number of lives")
+# Define characters
+lives_chr = "H"
+energy_chr = "E"
+shield_chr = "S"
+# Input values
+print("Please enter the number of lives.")
 lives = int(input())
-print("Please enter the energy level")
+print("Please enter the energy level.")
 energy = int(input())
-print("Please enter the shield level")
+print("Please enter the shield level.")
 shield = int(input())
+# Output results
 print()
 print("Health has been set.")
 print()
-print("Lives:  ")
-print("Energy: ")
-print("Shield: ")
+print("Lives:  ", lives_chr * lives)
+print("Energy: ", energy_chr * energy)
+print("Shield: ", shield_chr * shield)
