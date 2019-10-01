e38c6e29f31c612c32d3942660ad8c78b8a87a46
SteveMaher <4mahes86@solent.ac.uk>
2019-10-01 11:23:53 +0000

    nested

---
+++ 0-setup/1-basics/3-decisions/7--nested-decision/bot.py
@@ -0,0 +1,7 @@
+# ask for the type of book cover
+cover = str(input('What is the cover type of the book (hard / soft?'))
+
+# Hard or Soft cover
+if (cover == "hard"):
+
+    # ask if the cover is perfect bound
