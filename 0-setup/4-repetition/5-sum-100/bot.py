59d01c3235454fe8308bfa68f054f14c11f7d3d0
SteveMaher <4mahes86@solent.ac.uk>
2019-10-08 10:32:43 +0000

    Added code to demonstrate the use of whil loop to sum

---
+++ 0-setup/4-repetition/5-sum-100/bot.py
@@ -0,0 +1,12 @@
+# Sum of the first 100 numbers
+print("Calculating the sum of the first 100 numbers...")
+
+# variables
+total = 0
+number = 1
+
+while number <= 100:
+    total += number
+    number += 1
+
+print("...Done! The answer is",total)
