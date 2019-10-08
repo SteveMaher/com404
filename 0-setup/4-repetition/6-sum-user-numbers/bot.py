a155d52ffc236ceaba755c7e86cfc016a2bb0f66
SteveMaher <4mahes86@solent.ac.uk>
2019-10-08 11:27:44 +0000

    not working

---
+++ 0-setup/4-repetition/6-sum-user-numbers/bot.py
@@ -0,0 +1,16 @@
+# Sum user numbers
+# variables
+number = int(input("How many numbers should I sum up?"))
+count = 1
+total = 0
+
+while total <= number:
+    num = int(input("Please enter number" + str(count) + "of" + str(number) + ":")
+    count =
+
+
+
+
+
+
+print("The answer is ",total)
