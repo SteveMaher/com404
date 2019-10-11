1d1012d285f83f29bf8b9f538db303b6d78d61d4
SteveMaher <4mahes86@solent.ac.uk>
2019-10-11 15:08:12 +0000

    What strange markings do you see?

---
+++ 0-setup/1-basics/4-repetition/2-for-loop/4-characters/bot.py
@@ -0,0 +1,7 @@
+# Character string
+
+characters = str(input("What strange markings do you see?"))
+
+for position in range(0, len(characters),1):
+    print("\n index: " + characters[position])
+("Done !")
