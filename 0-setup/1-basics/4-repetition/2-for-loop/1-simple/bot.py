f6cc1b0d19e57e59256e190e7d4fb5a4eee13cdc
SteveMaher <4mahes86@solent.ac.uk>
2019-10-11 14:16:22 +0000

    Added code to demonstrate the use of a simple for loop

---
+++ 0-setup/1-basics/4-repetition/2-for-loop/1-simple/bot.py
@@ -0,0 +1,15 @@
+# For Loop to display an input number of pictures
+# input
+num = int(input("How many mountains should I display?"))
+
+# for loop
+
+for count in range(0,num,1):
+    print("         __")
+    print("        /  \_  ")
+    print("       /^    \\")
+    print("      /  ^    \_")
+    print("    _/ ^ ^     ^\\")
+    print("  /  ^     ^     \\")
+
+print ("\n Done !)")
