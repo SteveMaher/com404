d79b2b38c7cf9ba0c390ca5fce21934185c56dff
SteveMaher <4mahes86@solent.ac.uk>
2019-10-15 09:34:37 +0000

    # Code to demonstrate the use of a user-defined function with a nested decision

---
+++ 0-setup/1-basics/5-functions/2-nesting/bit.py
@@ -0,0 +1,15 @@
+
+# Code to demonstrate the use of a user-defined function with a nested decision
+# defining the identify listen
+def identify():
+    item = (input("What lies before us? "))
+    if item == "a large boulder":
+        print("\nIt's time to run!")
+    else:
+        print("\nWe will be fine")
+print()
+
+identify()
+
+
+
