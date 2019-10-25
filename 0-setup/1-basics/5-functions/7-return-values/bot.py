2c97623a327ae052ec23fa6fe5b6d8561d053f61
SteveMaher <4mahes86@solent.ac.uk>
2019-10-25 15:57:04 +0000

    updated 7

--- 0-setup/1-basics/5-functions/7-return-values/bot.py
+++ 0-setup/1-basics/5-functions/7-return-values/bot.py
@@ -1,27 +1,27 @@
 # Return Values
 # Function to sum the weights
 def sum_weights(beep, bop):
-    totalweight = beep + bop
-    return totalweight
-    print("The sum of Beep and Bop's weight is", sum_weights())
-
+    return beep + bop
 # Function to average the weights
 def calc_avg_weight(beep, bop):
-    averageweight = beep + bop / 2
-    return averageweight
-    print("The aveage weight of Beep and Bop is", calc_avg_weight())
-
+    total = sum_weights(beep, bop)
+    return total / 2
 # Function for user to input weights, decide if sum or average and the call correct function
 def run():
     beep = int(input('What is the weight of Beep?\n'))
     bop = int(input('What is the weight of Bop?\n'))
     question = input('What would you like to calculate (sum or average)?\n')
     if question == 'sum':
-        sum_weights(beep, bop)
+        print("The sum of Beep and Bop's weight is", sum_weights(beep, bop))
     elif question == 'average':
-        calc_avg_weight(beep, bop)
+        print("The aveage weight of Beep and Bop is", calc_avg_weight(beep, bop))
     else:
         print('Invalid entry, I can only sum or average. Please try again')
 
 # Run the program
 run()
+
+
+
+
+
