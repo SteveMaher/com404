66162248e206838be954b0140ed66b196751a93d
SteveMaher <4mahes86@solent.ac.uk>
2019-11-05 12:24:13 +0000

    Parent

---
+++ 0-setup/2-guis/1-classes-and-objects/FlyingBot.py
@@ -0,0 +1,3 @@
+class FlyingBot:
+    def __int__(self, hover_distance = 0):
+        self.hover_distance()
---
+++ 0-setup/2-guis/1-classes-and-objects/SuperBot.py
@@ -0,0 +1,3 @@
+class SuperBot:
+    def __int__(self, super_power_level = 0):
+        self.super_power_level()
