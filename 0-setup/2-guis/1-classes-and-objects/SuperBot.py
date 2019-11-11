2a92e0207dd7e9c7ee262ff9b7caecdd4429d9ca
SteveMaher <4mahes86@solent.ac.uk>
2019-11-11 12:04:45 +0000

    Superbot

--- 0-setup/2-guis/1-classes-and-objects/SuperBot.py
+++ 0-setup/2-guis/1-classes-and-objects/SuperBot.py
@@ -1,3 +1,9 @@
-class SuperBot:
-    def __int__(self, super_power_level = 0):
-        self.super_power_level()
+class Superbot(Bot):
+    def __init__(self, name, age, energy, shield_level, super_power_level =0):
+        super().__init__(name, age, energy, shield_level)
+        self.super_power_level = super_power_level
+        print()
+
+    def display_super_power_level(self):
+        print("Super Power level: " + str(self.super_power_level))
+        print()
