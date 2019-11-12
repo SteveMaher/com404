c567fe706a115bdecfdf1d1efd3ff6c5257d1f0a
Steven Maher <4mahes86@solent.ac.uk>
2019-11-12 11:32:06 +0000

    Added code to display a GUI using the place layout manager.

---
+++ 0-setup/3-Interfaces/2-guis/2-window-layouts/gui.py
@@ -0,0 +1,72 @@
+# GUI Interface
+from tkinter import *
+
+class Gui(Tk):
+    # initialise window
+    def __init__(self):
+        super().__init__()
+
+        #set windows attributes
+        self.title("Newsletter")
+        self.configure(bg="#eee", height=300, width=500)
+        
+        # add window components by
+        # ...creating an object of the component stored in an attribute
+        self.add_heading_label()
+        self.add_newsletter_label()
+        self.add_emailtext_label()
+        self.add_email_entry()
+        self.add_subscribe_button()
+
+
+
+    def add_heading_label(self):
+        # create
+        self.heading_label = Label()
+        self.heading_label.place(x=20, y=22)
+        # ...setting the attributes of the component using the attribute
+        self.heading_label.configure(font="Arial 24", text="RECEIVE OUR NEWSLETTER")
+
+    def add_newsletter_label(self):
+        # create
+        self.newsletter_label = Label()
+        self.newsletter_label.place(x=70, y=110)
+        # ...setting the attributes of the component using the attribute
+        self.newsletter_label.configure(font="Arial 11", text="Please enter your email below to receive our newsletter.")
+
+    def add_emailtext_label(self):
+        # create
+        self.emailtext_label = Label()
+        self.emailtext_label.place(x=70, y=200)
+        # ...setting the attributes of the component using the attribute
+        self.emailtext_label.configure(font="Arial 11", text="Email:")
+
+    def add_email_entry(self):
+        # create
+        self.email_entry = Entry(width=30)
+        self.email_entry.place(x=130, y=200)
+        # ...setting the attributes of the component using the attribute
+        self.email_entry.configure(font="Arial 11")
+
+# button
+
+    def add_subscribe_button(self):
+        # create
+        self.subscribe = Button(width=45)
+        self.subscribe.place(x=20, y=270)
+        # ...setting the attributes of the component using the attribute
+        self.subscribe.configure(font="Arial 11", text="Subscribe")
+
+
+
+
+
+
+
+
+        # ...assigning any event handlers to the component
+        # handle events
+        # (callback functions to handle events will go here)
+
+
+
